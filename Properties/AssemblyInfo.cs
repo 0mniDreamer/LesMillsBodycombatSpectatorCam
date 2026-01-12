3d1ca7954226b66f8f291279b53c7c8bf856713d
OmniDreamer <75704897+0mniDreamer@users.noreply.github.com>
2026-01-12 18:25:53 +0000

    Update MelonGame attribute to remove specific game filter
    Removed game filter to allow compatibility with any game.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: MelonInfo(typeof(BodycombatSpectatorCam.SpectatorCamMod), "Spectator Camera", "1.0.0", "OmniDreamer")]
-// Remove game filter to work with any game - or update with correct name from MelonLoader log
-// [assembly: MelonGame("Odders Lab", "LES MILLS BODYCOMBAT")]
 [assembly: MelonGame(null, null)]
